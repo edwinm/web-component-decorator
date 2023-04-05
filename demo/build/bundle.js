3968c84ef7861549b0922c1c8de27b69e3b08a3b
Edwin Martin <edwin@bitstorm.org>
2023-04-05 21:27:11 +0200

    Different whitespace

--- demo/build/bundle.js
+++ demo/build/bundle.js
@@ -1,7 +1,7 @@
 (function () {
-    'use strict';
+  "use strict";
 
-    /*! *****************************************************************************
+  /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
 
     Permission to use, copy, modify, and/or distribute this software for any
@@ -16,36 +16,69 @@
     PERFORMANCE OF THIS SOFTWARE.
     ***************************************************************************** */
 
-    function __decorate(decorators, target, key, desc) {
-        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-        return c > 3 && r && Object.defineProperty(target, key, r), r;
-    }
+  function __decorate(decorators, target, key, desc) {
+    var c = arguments.length,
+      r =
+        c < 3
+          ? target
+          : desc === null
+          ? (desc = Object.getOwnPropertyDescriptor(target, key))
+          : desc,
+      d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function")
+      r = Reflect.decorate(decorators, target, key, desc);
+    else
+      for (var i = decorators.length - 1; i >= 0; i--)
+        if ((d = decorators[i]))
+          r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+  }
 
-    function __metadata(metadataKey, metadataValue) {
-        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(metadataKey, metadataValue);
-    }
+  function __metadata(metadataKey, metadataValue) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function")
+      return Reflect.metadata(metadataKey, metadataValue);
+  }
 
-    /*
+  /*
      web-component-decorator
      @copyright 2020 Edwin Martin
      @license MIT
      */
-    const t=Symbol();function r(t,r){return n=>{customElements.define(t,n,r);}}function n(r){return function(n,o,e){const c=e.value?"value":"set";return n.constructor[t]?(n.constructor.observedAttributes.push(r),n.constructor[t].set(r,e[c])):(n.constructor.observedAttributes=[r],n.constructor[t]=new Map([[r,e[c]]])),n.attributeChangedCallback=function(r,o,e){n.constructor[t].get(r).call(this,e,o);},e}}
+  const t = Symbol();
+  function r(t, r) {
+    return (n) => {
+      customElements.define(t, n, r);
+    };
+  }
+  function n(r) {
+    return function (n, o, e) {
+      const c = e.value ? "value" : "set";
+      return (
+        n.constructor[t]
+          ? (n.constructor.observedAttributes.push(r),
+            n.constructor[t].set(r, e[c]))
+          : ((n.constructor.observedAttributes = [r]),
+            (n.constructor[t] = new Map([[r, e[c]]]))),
+        (n.attributeChangedCallback = function (r, o, e) {
+          n.constructor[t].get(r).call(this, e, o);
+        }),
+        e
+      );
+    };
+  }
 
-    /**
-     * Demo of web-component-decorator
-     *
-     * Run `npm start` to see this demo in action.
-     *
-     * Run `npm run build-demo` to compile the source.
-     */
-    let MyButton = class MyButton extends HTMLElement {
-        constructor() {
-            super();
-            this.attachShadow({ mode: "open" });
-            this.shadowRoot.innerHTML = `
+  /**
+   * Demo of web-component-decorator
+   *
+   * Run `npm start` to see this demo in action.
+   *
+   * Run `npm run build-demo` to compile the source.
+   */
+  let MyButton = class MyButton extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open" });
+      this.shadowRoot.innerHTML = `
           <style>
             button {
               padding: 5px;
@@ -60,21 +93,27 @@
               <slot></slot>
           </button>
         `;
-        }
-        setIcon(icon, oldIcon) {
-            this.shadowRoot.getElementById("icon").setAttribute('src', `icons/${icon}-24px.svg`);
-        }
-    };
-    __decorate([
-        n("icon"),
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [String, String]),
-        __metadata("design:returntype", void 0)
-    ], MyButton.prototype, "setIcon", null);
-    MyButton = __decorate([
-        r("my-button"),
-        __metadata("design:paramtypes", [])
-    ], MyButton);
-
-}());
+    }
+    setIcon(icon, oldIcon) {
+      this.shadowRoot
+        .getElementById("icon")
+        .setAttribute("src", `icons/${icon}-24px.svg`);
+    }
+  };
+  __decorate(
+    [
+      n("icon"),
+      __metadata("design:type", Function),
+      __metadata("design:paramtypes", [String, String]),
+      __metadata("design:returntype", void 0),
+    ],
+    MyButton.prototype,
+    "setIcon",
+    null
+  );
+  MyButton = __decorate(
+    [r("my-button"), __metadata("design:paramtypes", [])],
+    MyButton
+  );
+})();
 //# sourceMappingURL=bundle.js.map
--- demo/build/src/index.d.ts
+++ demo/build/src/index.d.ts
@@ -1,10 +1,23 @@
-export declare function define(name: string, options?: ElementDefinitionOptions): (constructor: CustomElementConstructor) => void;
-export declare function attribute(attr: string): (target: CustomElement, propertyName: string, propertyDescriptor: PropertyDescriptor) => PropertyDescriptor;
+export declare function define(
+  name: string,
+  options?: ElementDefinitionOptions
+): (constructor: CustomElementConstructor) => void;
+export declare function attribute(
+  attr: string
+): (
+  target: CustomElement,
+  propertyName: string,
+  propertyDescriptor: PropertyDescriptor
+) => PropertyDescriptor;
 export interface CustomElement {
-    constructor: Function & {
-        observedAttributes?: string[];
-    };
-    attributeChangedCallback?(attributeName: string, oldValue: string, newValue: string): void;
-    connectedCallback?(): void;
-    disconnectedCallback?(): void;
+  constructor: Function & {
+    observedAttributes?: string[];
+  };
+  attributeChangedCallback?(
+    attributeName: string,
+    oldValue: string,
+    newValue: string
+  ): void;
+  connectedCallback?(): void;
+  disconnectedCallback?(): void;
 }
